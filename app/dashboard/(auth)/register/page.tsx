b98941fac04f55360d1cba6e0628a5db8877bdab
SyedUmair282 <umairsyed282@gmail.com>
2024-02-11 15:06:15 +0500

    realtime db io done

--- app/dashboard/(auth)/register/page.tsx
+++ app/dashboard/(auth)/register/page.tsx
@@ -1,15 +1,47 @@
-import React from 'react'
+"use client"
+import React, {useState} from 'react'
 import Image from 'next/image'
+// import { db } from '@/firebase/config'
+import { getDatabase, ref, set, query, orderByChild, equalTo, get, push, child } from "firebase/database";
+import { app } from '@/firebase/config';
+import { useRouter } from 'next/navigation';
 
 const Register = () => {
+  const [userData,setUserData] = useState<any>(null)
+  const [loading,setLoading] = useState<any>(false)
+  const [error,setError] = useState<any>(false)
+  const router = useRouter();
+
+  const register = async (e:any) => {
+    e.preventDefault();
+    setLoading(true);
+    const db = getDatabase(app);
+    const userQuery = query(ref(db, 'users'), orderByChild('email'), equalTo(userData.email));
+    const snapshot = await get(userQuery);
+    if(snapshot.val()===null){
+      const newUserKey = push(child(ref(db), "users")).key;
+      await set(ref(db, "users/" + newUserKey), {
+      username: userData?.username,
+      email: userData?.email,
+      password: userData?.password,
+    });
+    router.push("/dashboard");
+    }else{
+      alert("This email is already registered")
+    }
+    setLoading(false);
+    
+  }
+
   return (
     <div>
-      <form className='flex flex-col gap-4 w-80 mx-auto'>
+      <form className='flex flex-col gap-4 w-80 mx-auto' onSubmit={register}>
         <span className='text-3xl font-semibold text-center'>Register Yourself</span>
-        <input required placeholder='username' className='bg-transparent outline-none text-gray-300 border-2 border-solid border-gray-300 p-2 text-base font-bold rounded' type="text" name="username" id="username" />
-        <input required placeholder='email address' className='bg-transparent outline-none text-gray-300 border-2 border-solid border-gray-300 p-2 text-base font-bold rounded' type="email" name="email" id="email" />
-        <input required placeholder='password' className='bg-transparent outline-none border-2 border-solid border-gray-300 p-2 text-base font-bold rounded' type="password" name="password" id="password" />
-        <button className='bg-green-500 rounded p-2	border-0 text-xs'>Register</button>
+        <input required placeholder='username' onChange={(e)=>setUserData({...userData,username:e.target.value})} className='bg-transparent outline-none text-gray-300 border-2 border-solid border-gray-300 p-2 text-base font-bold rounded' type="text" name="username" id="username" />
+        <input required placeholder='email address' onChange={(e)=>setUserData({...userData,email:e.target.value})} className='bg-transparent outline-none text-gray-300 border-2 border-solid border-gray-300 p-2 text-base font-bold rounded' type="email" name="email" id="email" />
+        <input required placeholder='password' onChange={(e)=>setUserData({...userData,password:e.target.value})} className='bg-transparent outline-none border-2 border-solid border-gray-300 p-2 text-base font-bold rounded' type="password" name="password" id="password" />
+        <button type='submit' className='bg-green-500 rounded p-2	border-0 text-xs' disabled={loading}>{!loading ? "Register" : "Loading..."}</button>
+        {error && <span>Something went wrong!!</span>}
         <span className='text-sm text-center'>Already have an account? Signin</span>
         <div className='flex items-center justify-center'>
           <div className='border border-solid border-gray-300 flex-1'/>
